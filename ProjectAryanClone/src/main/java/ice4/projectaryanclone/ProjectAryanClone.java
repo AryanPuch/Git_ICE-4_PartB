7cd6365f6e72b859465b0f906d8b51daf9e624de
AryanPuch <puchimad@sheridancollege.ca>
2026-01-29 13:06:25 -0500

    Added some new changes for Part B.

---
+++ ProjectAryanClone/src/main/java/ice4/projectaryanclone/ProjectAryanClone.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ice4.projectaryanclone;
+
+/**
+ *
+ * @author Aryan Puchimada, January 2026
+ */
+public class ProjectAryanClone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public void someNewChanges() {
+        System.out.println("My name is Aryan.");
+        System.out.println("This is my ICE 4 Part B!!");
+    }
+}
